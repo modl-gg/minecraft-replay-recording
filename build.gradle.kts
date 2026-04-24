b4c00a765d1b79bb41064ba7513920bc553ea4e6
byteful <info@byteful.me>
2026-04-23 23:10:07 -0400

    entitytype fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,10 @@ plugins {
     `maven-publish`
 }
 
-val packetEventsVersion = "2.12.3"
+val packetEventsVersion = "2.12.9"
 
 group = "gg.modl.minecraft.replay"
-version = "1.1.2"
+version = "1.1.3"
 
 java {
     toolchain {
